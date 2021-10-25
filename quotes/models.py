fab0e423034e623fb7636f94d937932ba538a221
mutemip <paulmutemi2030@gmail.com>
2021-10-26 01:34:04 +0300

    model modified

--- quotes/models.py
+++ quotes/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Quote(models.Model):
     quote_author = models.CharField(max_length=55)
-    quote_body = models.TextField(blank=True, null=True)
-    context = models.CharField(max_length=55)
-    source = models.CharField(max_length=55)
+    quote_body = models.TextField()
+    context = models.CharField(max_length=255, blank=True)
+    source = models.CharField(max_length=155)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.quote_author)
+        return self.quote_author
