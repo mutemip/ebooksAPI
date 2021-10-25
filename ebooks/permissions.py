03b7c0da7532bca7874b9e961f0e574758968d5c
mutemip <paulmutemi2030@gmail.com>
2021-10-25 23:28:05 +0300

    validation for one review per user

--- ebooks/permissions.py
+++ ebooks/permissions.py
@@ -5,3 +5,4 @@ class IsAdminUserOrReadOnly(permissions.IsAdminUser):
     def has_permission(self, request, view):
         is_admin = super().has_permission(request, view)
         return request.method in permissions.SAFE_METHODS or is_admin
+
--- ebooks/views.py
+++ ebooks/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from rest_framework.generics import get_object_or_404
 from rest_framework import permissions
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 from .permissions import IsAdminUserOrReadOnly
 
 from .models import Ebook, Reveiw
@@ -23,16 +24,26 @@ class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class ReviewCreateAPIView(generics.CreateAPIView):
     queryset = Reveiw.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get("ebook_pk")
         ebook = get_object_or_404(Ebook, pk=ebook_pk)
-        serializer.save(ebook=ebook)
+
+        review_author = self.request.user
+
+        # to make sure user only reviews an ebook once
+        review_queryset = Reveiw.objects.filter(ebook=ebook, review_author=review_author)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this Ebook!")
+
+        serializer.save(ebook=ebook, review_author=review_author)
 
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Reveiw.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 # class EbookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
 #     queryset = Ebook.objects.all()
